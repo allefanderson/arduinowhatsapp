4a23b272c73f658f690bec3ce776f8079f4b2a69
ALLEF ANDERSON DA SILVA <39091232+allefanderson@users.noreply.github.com>
2019-09-24 22:34:53 -0300

    Create whats.py

---
+++ whats.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+import os
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/led", methods=['POST'])
+def sms_reply():
+    
+    msg = request.form.get('Body')
+
+    resp = MessagingResponse()
+    if(msg == "Acender"):
+    	resp.message("Led ON")
+    	os.system("python on.py")
+
+    if(msg == "Apagar"):
+    	resp.message("Led Off")
+    	os.system("python off.py")
+
+    if(msg != "Acender" and msg != "Apagar"):
+    	resp.message("Digite Acender para LED ON\nE Apagar para LED OFF")
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
