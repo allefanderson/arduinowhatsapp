36e3d9dd7a84e76e2c7f63f932055d7207e3b927
ALLEF ANDERSON DA SILVA <39091232+allefanderson@users.noreply.github.com>
2019-09-24 22:44:52 -0300

    Update whats.py

--- whats.py
+++ whats.py
@@ -2,10 +2,7 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 import os
 
-
 app = Flask(__name__)
-
-
 
 @app.route("/led", methods=['POST'])
 def sms_reply():
